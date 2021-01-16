85386f217e324bd413e993cb450d73af32cdf55b
锅岛霖懿 <guodaolinyi@qq.com>
2021-01-16 20:42:50 +0800

    first

---
+++ dingtalk/domain/ChildDto.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * 结果数据
+ * @author auto create
+ */
+class ChildDto
+{
+	
+	/** 
+	 * 头像
+	 **/
+	public $avatar;
+	
+	/** 
+	 * 孩子信息
+	 **/
+	public $bind_students;
+	
+	/** 
+	 * 孩子nick
+	 **/
+	public $nick;
+	
+	/** 
+	 * 孩子对应的openId
+	 **/
+	public $open_id;
+	
+	/** 
+	 * 孩子家庭userid
+	 **/
+	public $userid;	
+}
+?>
